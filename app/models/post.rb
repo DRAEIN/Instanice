8a465b82eb2fbd6c637190f65d5ca153e4aef0aa
Mark Billes <mark.billes@web.de>
2017-05-14 17:34:18 +0200

    added ajax style javascript and final css

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :image, presence: true
   validates :user_id, presence: true
+  validates_length_of :comment, :minimum => 3, :maximum => 300
 
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
